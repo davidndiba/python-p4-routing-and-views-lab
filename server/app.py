9b37ac58dee96f4f1862fb5f98cc9cfa8b6baa5d
David <david.ndiba@student.moringaschool.com>
2023-09-20 03:00:37 +0300

    views labs

--- server/app.py
+++ server/app.py
@@ -6,3 +6,43 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return f'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(map(str, range(1, param + 1)))
+    return numbers
+
+@app.route('/math/<float:num1><string:operation><float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division by zero is not allowed!"
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return "Modulo by zero is not allowed!"
+
+    if result is not None:
+        return f'Result of {num1} {operation} {num2} is: {result}'
+    else:
+        return "Invalid operation or operands."
